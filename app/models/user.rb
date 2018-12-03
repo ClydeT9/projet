ae282877ce727ef8b28d5ab445a6440158f9fac7
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-03 16:53:43 +0100

    Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,12 @@ class User < ApplicationRecord
 
          validates :fullname, presence: true, length: {maximum: 75}
 
-         def self.new_with_session(params, session)
-          super.tap do |user|
-            if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-              user.email = data["email"] if user.email.blank?
-            end
-          end
-        end
-        
-        def self.from_omniauth(auth)
-          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.email = auth.info.email
-            user.password = Devise.friendly_token[0,20]
-            user.name = auth.info.name   # assuming the user model has a name
-          end
+         def self.create_from_omniauth(params)
+          user = find_or_create_by(email: params.info.email, uid: params.uid)
+          user.update({
+            token: params.credentials.token,
+            name: params.info.name
+          })
+          user
         end
 end
