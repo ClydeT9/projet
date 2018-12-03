415e582715c5269d6113280d9130a6c0dfb0376e
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-03 16:21:18 +0100

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,14 @@ class User < ApplicationRecord
 
          validates :fullname, presence: true, length: {maximum: 75}
 
-def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.password = Devise.friendly_token[0,20]
-  end
-end
+         def self.from_omniauth(auth)
+          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+            user.email = auth.info.email
+            user.password = Devise.friendly_token[0,20]
+            user.name = auth.info.name   # assuming the user model has a name
+            # If you are using confirmable and the provider(s) you use validate emails, 
+            # uncomment the line below to skip the confirmation emails.
+            # user.skip_confirmation!
+          end
+        end
 end
