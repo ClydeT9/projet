a0ac557d0d8fdea9b6d764e7fd025b173010f5e0
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-03 22:56:37 +0100

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
     # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
     # before the location can be stored.
     before_action :authenticate_user!
+    before_action :correct_domain!
+
   
     private
+    def correct_domain!
+      unless request.host == 'https://www.uneapp.com'
+        redirect_to root_url, :status => 301  # or explicitly 'http://www.mysite.com/'
+      end
+    end
       # Its important that the location is NOT stored if:
       # - The request method is not GET (non idempotent)
       # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
