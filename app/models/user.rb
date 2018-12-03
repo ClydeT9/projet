056c65c7fcdc3166ec084efd49c52b7d162ad0d5
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-03 16:18:18 +0100

    Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
          validates :name, presence: true, length: {maximum: 75} 
 
          validates :fullname, presence: true, length: {maximum: 75}
-         
-         def self.from_omniauth(access_token)
-          data = access_token.info
-          user = User.where(:email => data["email"]).first
-        
-          unless user
-            password = Devise.friendly_token[0,20]
-            user = User.create(name: data["name"], email: data["email"],
-              password: password, password_confirmation: password
-            )
-          end
-          user
-        end
+
+def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.password = Devise.friendly_token[0,20]
+  end
+end
 end
