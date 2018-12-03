6743025c6d9a184f098263ef0355d2a59c1791f8
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-03 23:55:17 +0100

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+UserMailer.signup_confirmation(@user).deliver
+
     def show
       @user = User.friendly.find(params[:id])
       @softwares = @user.softwares
       @saved = Software.joins(:likes).where("likes.user_id=?", current_user.id) if @user == current_user
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      UserMailer.signup_confirmation(@user).deliver
+      redirect_to @user, notice: "Succès"
+    else
+      render :new
+    end
   end
 end
