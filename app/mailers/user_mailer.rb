2d69422a1e38de61425c96e075fff5cf37fca631
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-04 01:52:40 +0100

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,19 @@
 class UserMailer < ActionMailer::Base
-    default from: "clyde.thomson@uneapp.com"
   
-    def welcome_email(user)
-      @user = user
-from = SendGrid::Email.new(email: 'clyde.thomson@uneapp.com')
-to = SendGrid::Email.new(email: 'clyde.thomson@uneapp.com')
-subject = 'Sending with SendGrid is Fun'
-content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-mail = SendGrid::Mail.new(from, subject, to, content)
+	def welcome_email(user)
+        from = Email.new(email: 'clyde.thomson@uneapp.com')
+        subject = 'Hello World from the SendGrid Ruby Library'
+        to = Email.new(email: 'clyde.thomson@uneapp.com')
+        content = Content.new(type: 'text/plain', value: 'some text here')
+        mail = SendGrid::Mail.new(from, subject, to, content)
+        # puts JSON.pretty_generate(mail.to_json)
+        puts mail.to_json
+      
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        puts response.status_code
+        puts response.body
+        puts response.headers
+      end
 
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail.to_json)
-puts response.status_code
-puts response.body
-puts response.parsed_body
-puts response.headers
-    end
   end
