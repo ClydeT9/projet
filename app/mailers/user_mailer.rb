3a4dd98b20a1c72fee79714308c0a4acfd82f980
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-04 15:19:42 +0100

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
-    mail(to: '@user.email', subject: 'Merci pour votre inscription', body: 'email body', template_id: 'd-51a204342561426da7b4527a840f8392')
+    mail( :to => @user.email,
+    :subject => 'Merci pour votre inscription' )
   end
 end
