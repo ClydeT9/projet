90859e5f8095435ac3e816583030a9038b6abf2d
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-04 02:11:57 +0100

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,14 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.new(user_params)
-    
-      respond_to do |format|
-        if @user.save
-    
-          # Sends email to user when user is created.
-          UserMailer.welcome_email(@user).deliver
-    
-          format.html { redirect_to @user, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @user }
-        else
-          format.html { render :new }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+      # Create the user from params
+      @user = User.new(params[:user])
+      if @user.save
+        # Deliver the signup email
+        UserMailer.send_signup_email(@user).deliver
+        redirect_to(@user, :notice => 'User created')
+      else
+        render :action => 'new'
       end
     end
-
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,8 @@
-class UserMailer < ActionMailer::Base
-  
-	def welcome_email(user)
-        from = Email.new(email: 'clyde.thomson@uneapp.com')
-        subject = 'Hello World from the SendGrid Ruby Library'
-        to = Email.new(email: 'clyde.thomson@uneapp.com')
-        content = Content.new(type: 'text/plain', value: 'some text here')
-        mail = SendGrid::Mail.new(from, subject, to, content)
-        # puts JSON.pretty_generate(mail.to_json)
-        puts mail.to_json
-      
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-        puts response.status_code
-        puts response.body
-        puts response.headers
-      end
+class UserMailer < ApplicationMailer
+    default from: 'clyde.thomson@uneapp.com'
 
+    def send_signup_email(user)
+      @user = user
+      mail(to: @user.email, subject: "Welcome to our app!")
+    end
   end
