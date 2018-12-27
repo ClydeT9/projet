d463dead0d41a26a573a5fbeb12b2130f42fe7d0
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-27 12:44:33 +0100

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ApplicationRecord
             user.email = auth.info.email
             user.password = Devise.friendly_token[0,20]
             user.name = auth.info.first_name
+            user.fullname = auth.info.last_name
           end
         end
 end
