5dc589b82dca3500f98992a4f61dddd5b8f8de12
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-27 20:42:07 +0100

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,11 @@ class User < ApplicationRecord
 
          extend FriendlyId
          friendly_id :name, use: [:slugged, :finders]
-         
+
          def self.create_from_provider_data(provider_data)
           where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
             user.email = provider_data.info.email
             user.password = Devise.friendly_token[0, 20]
-            user.skip_confirmation!
           end
         end
 end
