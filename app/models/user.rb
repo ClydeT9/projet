8a13aa76b87706a1cd9e04e4553887f7630aae3b
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-27 20:38:36 +0100

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
          def thumbnail input
           return self.images[input].variant(resize: '100x100>').processed
@@ -17,19 +17,12 @@ class User < ApplicationRecord
 
          extend FriendlyId
          friendly_id :name, use: [:slugged, :finders]
-
-         def self.new_with_session(params, session)
-          super.tap do |user|
-            if data = session["devise.facebook_data"] &&  session["devise.facebook_data"]["extra"]["raw_info"]
-              user.email = data["email"] if user.email.blank?
-            end
-          end
-        end
-        
-        def self.from_omniauth(auth)
-          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.email = auth.userinfo.email
-            user.password = Devise.friendly_token[0,20]
+         
+         def self.create_from_provider_data(provider_data)
+          where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+            user.email = provider_data.info.email
+            user.password = Devise.friendly_token[0, 20]
+            user.skip_confirmation!
           end
         end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,13 +262,13 @@ Devise.setup do |config|
   ENV['FACEBOOK_KEY'],
   ENV['FACEBOOK_SECRET'],
   callback_url:  "https://www.uneapp.com/my_engine/users/auth/facebook/callback",
-  scope: 'email', info_fields: 'email, first_name, last_name'
+  scope: 'public_profile,email'
 
   config.omniauth :google_oauth2,
   ENV['GOOGLE_KEY'],
   ENV['GOOGLE_SECRET'],
   callback_url:  "https://www.uneapp.com/my_engine/users/auth/google_oauth2/callback",
-  scope: 'email', info_fields: 'email'
+  scope: 'userinfo.email,userinfo.profile'
   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
