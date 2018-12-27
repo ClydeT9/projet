074cf23f8bc14888c9e59cc01d7dca5d7e891381
Clyde Thomson <thomson.clyde@outlook.com>
2018-12-27 21:56:46 +0100

    google

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,12 @@ class User < ApplicationRecord
             user.password = Devise.friendly_token[0, 20]
           end
         end
-        
+
         def self.create_from_google_data(google_data)
           where(provider: google_data.provider, uid: google_data.uid).first_or_create do | user |
             user.email = google_data.info.email
             user.password = Devise.friendly_token[0, 20]
         end
-      
+      end
 
 end
