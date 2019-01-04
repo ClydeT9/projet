7bc44c9906bd03f725e6c77db4d4c3ad5810f61a
Clyde Thomson <thomson.clyde@outlook.com>
2019-01-04 01:00:59 +0100

    new

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.create_from_facebook_data(request.env['omniauth.auth'])
+    if @user.save
+      UserMailer.send_signup_email(@user).deliver
+      UserMailer.send_signup_admin(@user).deliver
+    end
     if @user.persisted?
-      if @user.save
-        UserMailer.send_signup_email(@user).deliver
-        UserMailer.send_signup_admin(@user).deliver
-      end
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
@@ -16,11 +16,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.create_from_google_data(request.env['omniauth.auth'])
+    if @user.save
+      UserMailer.send_signup_email(@user).deliver
+      UserMailer.send_signup_admin(@user).deliver
+    end
     if @user.persisted?
-      if @user.save
-        UserMailer.send_signup_email(@user).deliver
-        UserMailer.send_signup_admin(@user).deliver
-      end
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
     else
