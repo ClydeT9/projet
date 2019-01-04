6b5b74a28320c50ea8bf45198efe47d726a09558
Clyde Thomson <thomson.clyde@outlook.com>
2019-01-04 01:06:47 +0100

    new

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.create_from_facebook_data(request.env['omniauth.auth'])
-    if @user.save
-      UserMailer.send_signup_email(@user).deliver
-      UserMailer.send_signup_admin(@user).deliver
-    end
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
@@ -16,10 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.create_from_google_data(request.env['omniauth.auth'])
-    if @user.save
-      UserMailer.send_signup_email(@user).deliver
-      UserMailer.send_signup_admin(@user).deliver
-    end
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
